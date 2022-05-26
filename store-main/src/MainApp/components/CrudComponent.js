c1c77f9ffa509ba2d7deaf38af050ed97a6a2c8c
Derin Owoade <owoadederin6@gmail.com>
2022-05-26 01:30:00 +0100

    Crud item Basic setup

--- store-main/src/MainApp/components/CrudComponent.js
+++ store-main/src/MainApp/components/CrudComponent.js
@@ -4,15 +4,39 @@ import axios from "axios";
 
 export default function CrudComponent(){
     const [itemData, setItemData] = React.useState()
-
+    const [resStatus, setResStatus] = React.useState()
+    const [ItemArray, setItemArray] = React.useState()
     const getData = async()=>{
-        const data = await axios.get("http://localhost:1000/storeV1").catch(err =>{
+        const response = await axios.get("http://localhost:1000/storeV1").catch(err =>{
             console.log(err)
         }) 
 
-        const res = await JSON.stringify(data)
-        console.log(res)
+        const res = await JSON.parse(JSON.stringify(response))
+        const {data} = res
+        const resItemArray = data.data
+        console.log(res.status)
+        setItemData(resItemArray)
+        console.log(resItemArray)
+        setResStatus(res.status)
     }
+
+    const CrudItemArray = []
+/* CrudItemArray = itemData.map(item => <CrudItem key={item._id} name={item.name}
+                price ={item.price} description = {item.description} img={item.img}
+                />) */
+
+    React.useEffect(()=>{
+        if(resStatus === 200){
+            setItemArray(itemData.map(item => <CrudItem key={item._id} name={item.name}
+                price ={item.price} description = {item.description} img={item.img}
+                />))
+      
+        } 
+        
+
+        
+        
+    }, [itemData, resStatus])
 
     React.useEffect(()=>{
         getData()
@@ -22,6 +46,6 @@ export default function CrudComponent(){
 
     return <div>
         <h1 className="pt-16">Crud Component</h1>
-        <CrudItem />
+        {ItemArray}
     </div>
 }
--- store-main/src/MainApp/components/CrudItem.js
+++ store-main/src/MainApp/components/CrudItem.js
@@ -1,12 +1,11 @@
 import React from "react";
 
-export default function CrudItem(){
+export default function CrudItem({name, price, img, description}){
     return <div className="flex max-w-screen">
         <div>
-           <input id="img" className="hidden"></input>
-           <label htmlFor="img">
-               Hey
-                 <img></img>
+           <input id="img" name="img" type="file" className="hidden"></input>
+           <label htmlFor="img" className="h-10">
+                 <img src={`data:image/jpeg;base64,${img}`} className="h-20"></img>{/* data:image/jpeg;base64, */}
            </label>
         </div>
 
